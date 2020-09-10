17863985d4d435fdbf2cd698f99613d309ab3ddf
Jenish Patel <hsinej@gmail.com>
2020-09-10 14:34:21 -0600

    Documentation completed for merge sort

--- lab_1/Jenish_Patel_MergeSort/src/MergeSort.java
+++ lab_1/Jenish_Patel_MergeSort/src/MergeSort.java
@@ -2,10 +2,17 @@
  * Lab 1: Java Basics, Merge Sort and Maven <br />
  * The {@code MergeSort} class
  * @author <Jenish Patel>
+ * @version <1.0>
  */
 public class MergeSort {
 
-    // More methods can be added here
+    /**
+     * Merges two arrays into a third array, in a way that the arrangement
+     * into the third array is in the ascending order
+     * @param left left array
+     * @param right right array
+     * @param numbers third array
+     */
 	public static void merge(int[] left,int[] right,int[] numbers) {
 		int lengthLeft = left.length;
 		int lengthRight = right.length;
@@ -13,6 +20,7 @@ public class MergeSort {
 		int j = 0;
 		int k = 0;
 		
+		// Traverses two arrays and lowest element from both arrays is placed into third array
 		while (i < lengthLeft && j < lengthRight) {
 			if (left[i] <= right[j]) {
 				numbers[k] = left[i];
@@ -24,11 +32,14 @@ public class MergeSort {
 			k += 1;
 		}
 		
+		// Traverses left array and places element into third array
 		while (i < lengthLeft) {
 			numbers[k] = left[i];
 			i += 1;
 			k += 1;
 		}
+		
+		// Traverses right array and places element into third array
 		while (j < lengthRight) {
 			numbers[k] = right[j];
 			j += 1;
@@ -37,12 +48,15 @@ public class MergeSort {
 	}
 	
     /**
-     * The merge sort procedure
+     * Breaks down array into subarrays recursively and then patches up the array
+     * via calling merge function
      * @param numbers   {@code int[]} The integer array to be sorted
+     * @return numbers the sorted array
      */
     public static int[] sort(int[] numbers) {
         int n = numbers.length; // Length of array
         
+        // Base case
         if (n < 2) {
         	return numbers;
         }
@@ -54,10 +68,12 @@ public class MergeSort {
         int[] left = new int[leftSize];
         int[] right = new int[rightSize];
         
+        // Copys elements from previous array into new array
         for (int i = 0; i < mid; i++) {
         	left[i] = numbers[i];
         }
         
+        // Copys elements from previous array into new array
         for (int i = mid; i < n; i++) {
         	right[i - mid] = numbers[i];
         }
@@ -71,7 +87,7 @@ public class MergeSort {
     }
 
     /**
-     * Main entry: test the HeapSort
+     * Main entry: test the MergeSort
      * @param args      {@code String[]} Command line arguments
      */
     public static void main(String[] args) {
