80ffc580a05c34ff778f094f9132350272b3dd14
Jenish Patel <hsinej@gmail.com>
2020-09-22 11:55:29 -0600

    DaysOld src file updated

--- DaysOld/src/DaysOld.java
+++ DaysOld/src/DaysOld.java
@@ -1,60 +1,64 @@
-import java.util.Calendar;
-import java.util.Date;
 import java.util.ArrayList;
-import java.util.regex.*;
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Assignment 1: Using standard libraries <br />
  * Calculate age in days
  */
 public class DaysOld {
 
-    /**
-     * Calculate how many days between today and the date, and them out
-     * @param birthday      {@code String} The start date
-     */
-    public static void days(String birthday) {
-        // TODO: Assignment 1 -- write your code here.
-    	int counter = 0;
-    	ArrayList<Integer> al = new ArrayList<Integer>(3);
-    	Calendar now = Calendar.getInstance();
-    	Calendar birth = Calendar.getInstance();
-    	String pattern = "[0-9]+";
-    	Pattern p = Pattern.compile(pattern);
-    	Matcher m = p.matcher(birthday);
-    	
-    	while (m.find()) {
-    		int num = Integer.parseInt(birthday.substring(m.start(), m.end()));
-    		al.add(num);
-    	}
-    	
-    	birth.set(al.get(0), al.get(1)-1, al.get(2));
-    	
-    	if (!now.after(birth)) {
-    		System.out.println("This is a wrong birthday");
-    	} else {
-    		while (now.get(Calendar.YEAR) != birth.get(Calendar.YEAR)
-        			|| now.get(Calendar.MONTH) != birth.get(Calendar.MONTH)
-        			|| now.get(Calendar.DAY_OF_YEAR) != birth.get(Calendar.DAY_OF_YEAR)) {
-        		birth.add(Calendar.DAY_OF_YEAR, 1);
+	public static ArrayList<Integer> changeFormat(String birthday) {
+		ArrayList<Integer> date = new ArrayList<Integer>(3);
+		String pattern = "[0-9]+";
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(birthday);
 
-        		counter += 1;
-        	}
-    		System.out.println(counter);
-    	}
-    
-    	
+		while (m.find()) {
+			int num = Integer.parseInt(birthday.substring(m.start(), m.end()));
+			date.add(num);
+		}
 
-    } // public static void days(String birthday)
+		return date;
+	}
 
-    /**
-     * Main entry
-     * @param args          {@code String[]} Command line arguments
-     */
-    public static void main(String[] args) {
-        //days("2000-1-1");
-        //days("3000-1-1");           // This is a wrong birthday
-        //Add your birthday
-        days("2020/9/17");
-    }
+	/**
+	 * Calculate how many days between today and the date, and them out
+	 * 
+	 * @param birthday {@code String} The start date
+	 */
+	public static void days(String birthday) {
+		int counter = 0;
+		Calendar now = Calendar.getInstance();
+		Calendar birth = Calendar.getInstance();
+		ArrayList<Integer> date = changeFormat(birthday);
+
+		birth.set(date.get(0), date.get(1) - 1, date.get(2)); // Month is 0-indexed
+
+		if (!now.after(birth)) {
+			System.out.println("This is a wrong birthday");
+		} else {
+			while (now.get(Calendar.YEAR) != birth.get(Calendar.YEAR)
+					|| now.get(Calendar.MONTH) != birth.get(Calendar.MONTH)
+					|| now.get(Calendar.DAY_OF_YEAR) != birth.get(Calendar.DAY_OF_YEAR)) {
+				birth.add(Calendar.DAY_OF_YEAR, 1);
+				counter += 1;
+			}
+			System.out.println(counter);
+		}
+	}
+
+	/**
+	 * Main entry
+	 * 
+	 * @param args {@code String[]} Command line arguments
+	 */
+	public static void main(String[] args) {
+		// days("2000-1-1");
+		// days("3000-1-1"); // This is a wrong birthday
+		// Add your birthday
+		days("1999/11/15");
+	}
 
 }
